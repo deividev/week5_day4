d0c62a504e3ee48204617ced54a53d9ab9a79397
Xamp <deivid.develop@gmail.com>
2020-02-03 11:55:02 +0100

    enter function added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const list = document.getElementById('list');
+
+const inputNode = document.getElementById('input');
+inputNode.addEventListener('keydown', addTodoEnter);
+
+
+
+
 function addToDo () {
     const li = document.createElement('li');
     const input = document.getElementById('input');
@@ -10,54 +18,60 @@ function addToDo () {
     const btnDelete = addDeleteBtnElement();
     const btnEdit = addEditBtnElement();
     const btnDone = addDoneBtnElement();
-
     li.appendChild(text);
     li.appendChild(btnDelete);
     li.appendChild(btnEdit);
     li.appendChild(btnDone);
     li.classList.add("task");
     ul.appendChild(li);
+    input.value = '';
 }
 function addDeleteBtnElement () {
     const btnDelete = document.createElement('button');
     btnDelete.appendChild(document.createTextNode('Delete'));
-    btnDelete.addEventListener("click", deleteElement, true)
+    btnDelete.addEventListener("click", deleteElement, {onde : true});
+    btnDelete.classList.add('action-btn');
     return btnDelete;
 }
 function addEditBtnElement () {
     const btnEdit = document.createElement('button');
     btnEdit.appendChild(document.createTextNode('Edit'));
-    btnEdit.addEventListener("click", editElement, true);
+    btnEdit.addEventListener("click", editElement, {onde : true});
+    btnEdit.classList.add('action-btn');
     return btnEdit;
 }
 function addDoneBtnElement () {
     const btnDone = document.createElement('button');
     btnDone.appendChild(document.createTextNode('Done'));
-    btnDone.addEventListener("click", doneElement, true)
+    btnDone.addEventListener("click", doneElement, {onde : true})
+    btnDone.classList.add('action-btn');
     return btnDone;
 }
 
 function addSaveBtnElement () {
     const btnSave = document.createElement('button');
     btnSave.appendChild(document.createTextNode('Save'));
-    btnSave.addEventListener("click", saveElement, true);
+    btnSave.addEventListener("click", saveElement, {onde : true});
+    btnSave.classList.add('action-btn');
     return btnSave;
 }
-function deleteElement(e) {
+function deleteElement(event) {
     //TODO Borrar el elemento li desde el cual se dispara el evento.
-    const li = e.target.parentElement;
-    const ul = li.parentElement;
-    ul.removeChild(li);
+    //Otra forma de hacerlo
+    //const li = e.target.parentElement;
+    //const ul = li.parentElement;
+    //ul.removeChild(li);
+    list.removeChild(event.currentTarget.parentElement);
 }
-function doneElement(e) {
+function doneElement(event) {
     //TODO Cambiar el color de fondo del elemento li desde el cual se dispara el evento de rojo a verde.
-    const li = e.target.parentElement;
     //li.style.background = "#02db3c"
+    const li = event.target.parentElement;
     li.classList.add('check');
-
+    event.target.parentElement.removeChild(event.currentTarget);
 }
-function editElement(e) {
-    const li = e.target.parentElement;
+function editElement(event) {
+    const li = event.target.parentElement;
     const arr = li.childNodes;
     const liText = li.childNodes[0];
     const btnDone = li.childNodes[1];
@@ -66,14 +80,14 @@ function editElement(e) {
     const btnSave = addSaveBtnElement();
     const input = document.createElement('input');
     input.setAttribute("id", "inputEdit");
+    input.value = liText.textContent;
     li.removeChild(liText);
     li.insertBefore(input, btnDone);
     li.removeChild(btnEdit);
     li.insertBefore(btnSave, btnDelete);
 }
-
-function saveElement(e) {
-    const li = e.target.parentElement;
+function saveElement(event) {
+    const li = event.target.parentElement;
     const input = li.childNodes[0];
     const inputValue = input.value;
     if (input.value === ''){
@@ -85,6 +99,13 @@ function saveElement(e) {
     const text = document.createTextNode(inputValue);
     li.insertBefore(text, input);
     li.removeChild(input);
+    //btnEdit.removeEventListener('click', editElement);
     li.insertBefore(btnEdit, btnSave);
     li.removeChild(btnSave);
+}
+
+function addTodoEnter(event) {
+    if (event.keyCode === 13){
+      addToDo();
+    } 
 }
